8edc8422b006cce9d0f0fb21bf4894c7cbdbffe6
Jimi Damon <jdamon@gmail.com>
2015-05-28 12:55:43 -0700

    ABle to multiply number with a constant

--- measurement.R
+++ measurement.R
@@ -7,12 +7,18 @@
 #
 # - the inverted matrix will only be updated if the main matrix
 #   is "dirtied" : if the underlying matrix has any values changed.
+
+library(gmp)
+
 as.measurement <- function(x,sd=NA,mean=NA,nsigfigs=NA,sep=" ") {
 
     num_sigfigs <- if ( is.na(nsigfigs) ) { NA } else { nsigfigs }
     
-    if ( is.na(mean) ) {
-
+    if ( length(x) >= 1 ) {
+        num_sigfigs <- if ( is.na(num_sigfigs) ) { min(sapply(x,sigfigs)) } else { num_sigfigs }
+        x_bar <- signif(mean(x),num_sigfigs )
+        x_sd <- if ( !is.na(sd) ) { sd } else { signif(sd(x),1) } 
+    } else if ( is.na(mean) ) {
         num_sigfigs <- if ( is.na(num_sigfigs) ) { min(sapply(x,sigfigs)) } else { num_sigfigs }
         x_bar <- signif(mean(x),num_sigfigs )
         x_sd <- signif(sd(x),1)
@@ -41,6 +47,10 @@ as.measurement <- function(x,sd=NA,mean=NA,nsigfigs=NA,sep=" ") {
     get_uncertainty <- function() {
         x_sd
     }
+    get_uncertainty_calc<- function() {
+        if ( is.na(x_sd) ) { 0 } else { x_sd } 
+    }
+    
     get_sigfigs <- function() { num_sigfigs }
     
     uncertainty <- function() {
@@ -55,7 +65,7 @@ as.measurement <- function(x,sd=NA,mean=NA,nsigfigs=NA,sep=" ") {
         cat(paste(signif(x_bar,num_sigfigs), uncertainty(),"\n"))
     }
     
-    tmp <- list(get=get,get_sigfigs=get_sigfigs,set_x=set_x,get_x=get_x,set_uncertainty=set_uncertainty,get_uncertainty=get_uncertainty,inspect=inspect)
+    tmp <- list(get=get,get_uncertainty_calc=get_uncertainty_calc,get_sigfigs=get_sigfigs,set_x=set_x,get_x=get_x,set_uncertainty=set_uncertainty,get_uncertainty=get_uncertainty,inspect=inspect)
     class(tmp) = 'measurement'
     tmp
 }
@@ -64,30 +74,35 @@ print.measurement = function(obj) {
     obj$inspect()
 }
 
+# Significant figures for multiplication and division should
+# be set to the minmium in a sequence of calculations
+#
 `*.measurement` = function(meas,omeas) {
-    as.measurement( mean=(meas$get_x() * omeas$get_x()),
-                   sd=(meas$get_x() * omeas$get_x())*(meas$get_uncertainty()/ meas$get_x() + omeas$get_uncertainty()/omeas$get_x()),
+    as.measurement( (meas$get_x() * omeas$get_x()),
+                   sd=(meas$get_x() * omeas$get_x())*(meas$get_uncertainty_calc()/ meas$get_x() + omeas$get_uncertainty_calc()/omeas$get_x()),
                    nsigfigs=min(meas$get_sigfigs(),omeas$get_sigfigs())
                    )
 }
 
 `/.measurement` = function(meas,omeas) {
-    as.measurement( mean=(meas$get_x() / omeas$get_x()),
-                   sd=(meas$get_x() * omeas$get_x())*(meas$get_uncertainty()/ meas$get_x() + omeas$get_uncertainty()/omeas$get_x()),
+    as.measurement( (meas$get_x() / omeas$get_x()),
+                   sd=(meas$get_x() / omeas$get_x())*(meas$get_uncertainty_calc()/ meas$get_x() + omeas$get_uncertainty_calc()/omeas$get_x()),
                    nsigfigs=min(meas$get_sigfigs(),omeas$get_sigfigs())
                    )
 }
 
+# Significant figures for addition and subtraction should
+# be set to the maximum of a sequence of calculations
 `+.measurement` = function(meas,omeas) {
-    as.measurement( mean=(meas$get_x() + omeas$get_x()),
-                   sd=(meas$get_uncertainty() + omeas$get_uncertainty() ),
+    as.measurement( (meas$get_x() + omeas$get_x()),
+                   sd=(meas$get_uncertainty_calc() + omeas$get_uncertainty_calc() ),
                    nsigfigs=max(meas$get_sigfigs(),omeas$get_sigfigs())
                    )
 }
 
 `-.measurement` = function(meas,omeas) {
-    as.measurement( mean=(meas$get_x() - omeas$get_x()),
-                   sd=(meas$get_uncertainty() + omeas$get_uncertainty() ),
+    as.measurement( meas$get_x() - omeas$get_x(),
+                   sd=(meas$get_uncertainty_calc() + omeas$get_uncertainty_calc() ),
                    nsigfigs=max(meas$get_sigfigs(),omeas$get_sigfigs())
                    )
 }
